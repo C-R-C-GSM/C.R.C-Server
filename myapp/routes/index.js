d1677a35fec23ed3463ce9fd7c09952760fd8472
ingyocode <69563418+ingyocode@users.noreply.github.com>
2021-03-08 11:53:54 +0900

    project goal
    project goal

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+//데이터 받아서 프론트에 보내주기
+
+
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
