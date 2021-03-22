badc4ef7476db1c780fc6efdb149c80514863c79
ingyocode <69563418+ingyocode@users.noreply.github.com>
2021-03-22 13:56:02 +0900

    add array
    add array

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -2,10 +2,26 @@ var express = require('express');
 var router = express.Router();
 
 
+let students = new Array(210);
+
+
+
 router.get('/', function(req, res, next) {
   console.log('get success');
-  res.render('index', { title: 'Express' });
-  
+  res.render('index');
+});
+
+
+router.post('/', function(req,res,next) {
+  console.log('post success');
+  for (let index = 0; index < students.length; index++) {
+    students[index] = req.body.scan;
+    req.body.scan = null;
+    console.log(students[index]);
+    while(req.body.scan == null);
+    console.log(students[index]);
+  }
+  console.log('post end');
 });
 
 module.exports = router;
