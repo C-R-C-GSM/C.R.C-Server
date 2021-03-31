658965d53950113c80a3011bc4c989eeb61b7d12
ingyocode <69563418+ingyocode@users.noreply.github.com>
2021-04-01 08:58:43 +0900

    add socket
    add socket communication

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+
 require('dotenv').config();
+
 var bodyParser = require('body-parser');
 // app.use(bodyParser.urlencoded({extended:true}));
 // app.use(bodyParser.json());
+
+//var server = require('http').createServer(app);
+//var io = require('socket.io')(server);
 
 let students = new Array(210);
 
@@ -20,33 +25,20 @@ var connection = mysql.createConnection({
 
 connection.connect();
 
-//let student = 0;
 //나중엔 DB에서 값 가져오기
-//학생의 count수
-
-let num = 0;  //if문에 사용되기 위한 변수.
+let student = 0;  //if문에 사용되기 위한 변수.
 
 router.get('/', function(req, res, next) {
   console.log('get success');
-  res.render('index', {student: student});  //페이지와 변수는 맨 처음에만 준다.
-  num = student;
-  while(TRUE) {
-    if(student != num) {
-      num = student;
-      res.render({student: student}); //페이지는 이미 보내졌기 때문에 변수만 보낸다.
-    } else {
-      setTimeout(function() {}, 1000);
-    }
-  }
-
+  res.json({student:student});
 });
 
 router.post('/', function(req,res,next) {
-  num = req.body.counter;
+  student = req.body.counter;
   console.log(req.body);
   console.log('post success');
-  /*  나중에 쓸 쿼리문
-  connection.query("SELECT * FROM student", function(error, results, fields) {
+  /*  나중에 쓸 쿼리문 1. 학생의 번호를 가져오는 쿼리문 2. 학생의 체크를 모두 가져오는 것
+  connection.query("SELECT students FROM student", function(error, results, fields) {
     console.log(results[0].number);
     res.send(results[0].number);
   });
@@ -54,9 +46,13 @@ router.post('/', function(req,res,next) {
     console.log(results[0].check);
   });
   */
-  res.render('index', {num:num});  //페이지와 변수는 맨 처음에만 준다.
-  console.log('post end');
 });
+
+// io.on('connection', function(socket) {
+//   socket.on('event_name', function(data) {
+//     console.log('Message from Client: ' + data);
+//   });
+// });
 
 module.exports = router;
  
