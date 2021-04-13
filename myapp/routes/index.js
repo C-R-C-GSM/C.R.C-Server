afcd3b7c69c27d9105b08d149f2b8245e2e8cec0
ingyocode <69563418+ingyocode@users.noreply.github.com>
2021-04-13 20:57:29 +0900

    add replace and join but fail
    add replace and join but fail

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -42,7 +42,7 @@ client.fetch("http://gsm.gen.hs.kr/xboard/board.php?tbnum=8", {}, function (err,
 for (; week <= 6; week++) {
   for (; day <= 6; day++) {
     arr[count] = $(`#xb_fm_list > div.calendar > ul:nth-child(${week}) > li:nth-child(${day}) > div > div.slider_food_list`).text();
-    console.log(arr[count]);
+    // console.log(arr[count]);
     for (let index = 0; index < arr.length; index++) {
       if(arr[index]=='\r\n') {
         arr[index] = 0;
@@ -51,10 +51,13 @@ for (; week <= 6; week++) {
     }
   }
 }
+
+// const text = arr.join('');
+// console.log(text.replace('\n', ''));
 const set = new Set(arr);
 const uni = [...set];
-console.log(set[1]);
-//console.log(meals);  
+console.log(set[0]);
+console.log(meals);  
 });
 
 
