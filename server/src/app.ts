35a8d2b1fc66ba810f5b033a125495e36b5de2b3
ingyocode <69563418+ingyocode@users.noreply.github.com>
2021-04-26 16:51:02 +0900

    change success
    without error

--- server/src/app.ts
+++ server/src/app.ts
@@ -10,25 +10,20 @@ let meal_text;
 let meal_text_split = new Array();
 let school_meal_arr = new Array();
 
-interface Data {
-    err: string
-}
 
-//#region  socket io
-// const socketapp = require('express'); 
-// const http = require('http').Server(socketapp); 
-// const io = require('socket.io')(http);
-// const room = io.of('/test');
+const socketapp = require('express'); 
+const http = require('http').Server(socketapp); 
+const io = require('socket.io')(http);
+const room = io.of('/test');
 
-// http.listen(9000, function () { console.log('Listening on *:9000'); });
-//#endregion
+http.listen(9000, function () { console.log('Listening on *:9000'); });
 
 const mysql = require("mysql");
 
 var bodyParser = require('body-parser');
 var client = require('cheerio-httpcli');
 
-client.fetch("http://gsm.gen.hs.kr/xboard/board.php?tbnum=8", {}, function (err, $, res, body) {
+client.fetch("http://gsm.gen.hs.kr/xboard/board.php?tbnum=8", {}, function (err:Error, $:any, res:Response, body:Body) {
 
   for (let week = 2; week <= 6; week++) {
     for (let day = 2; day <= 6; day++) {
@@ -49,9 +44,32 @@ client.fetch("http://gsm.gen.hs.kr/xboard/board.php?tbnum=8", {}, function (err,
   console.log(school_meal_arr);
 });
 
+let student = 0;
+
 app.get("/", (request: Request, response: Response, next: NextFunction) => {
-  response.send("ts");
+    console.log('get success');
+    response.json({student:student});
 });
+
+app.post('/', function(request:Request,response: Response,next:NextFunction) {
+    student = request.body.counter;
+    console.log(request.body.counter);
+    console.log('post success');
+    /*  나중에 쓸 쿼리문 1. 학생의 번호를 가져오는 쿼리문 2. 학생의 체크를 모두 가져오는 것
+    connection.query("SELECT students FROM student", function(error, results, fields) {
+      console.log(results[0].number);
+      res.send(results[0].number);
+    });
+    connection.query("SELECT students FROM student", function(error, results, fields) {
+      console.log(results[0].check);
+    });
+    */
+  //  connection.query("INSERT INTO students(check) VALUE('"+true+"'", function(error, results, fields) {
+  //    if(error) {
+  //      console.log(error);
+  //    }
+  //  });
+  });
 
 app.listen(3000, () => {
   console.log("start");
